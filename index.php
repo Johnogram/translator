f6762e0f7757128678b8fd34e1c1457aeb91ed85
John Minns <john@john-o-gram.co.uk>
2018-02-19 18:42:38 +0000

    Barebones in place

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    require_once("includes/init.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Translator P.O.C</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script defer src="assets/js/main.min.js"></script>
+    </head>
+    <body>
+
+    </body>
+</html>
