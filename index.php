4011b6604b31d7f5237e400771f1fbc87694e5b2
John Minns <john@john-o-gram.co.uk>
2018-02-19 18:57:34 +0000

    Page layout done

--- index.php
+++ index.php
@@ -13,5 +13,53 @@
     </head>
     <body>
 
+        <!-- HERO -->
+        <section class="hero is-primary is-medium">
+
+            <!-- HERO BODY -->
+            <div class="hero-body">
+                <div class="container has-text-centred">
+                    <h2 class="subtitle">
+                        Select your language below
+                    </h2>
+                </div>
+            </div>
+            <!--/HERO BODY -->
+
+            <!-- HERO FOOTER -->
+            <div class="hero-foot">
+                <nav class="tabs">
+                    <div class="container">
+                        <ul>
+                            <li><a href="#">English</a></li>
+                            <li><a href="#">Francais</a></li>
+                            <li><a href="#">Deutsch</a></li>
+                            <li><a href="#">Pirate, argh!</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <!--/HERO FOOTER -->
+
+        </section>
+        <!--/HERO -->
+
+        <!-- FORM -->
+        <section class="section is-medium">
+            <div class="container">
+                <h1 class="title">Welcome to our app!</h1>
+                <p style="margin-bottom: 1em;">Please fill in this form</p>
+                <div class="field is-grouped">
+                    <div class="control">
+                        <a href="#" class="button is-success">Next</a>
+                    </div>
+                    <div class="control">
+                        <a href="#" class="button is-warning">Submit</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--/FORM -->
+
     </body>
 </html>
